23291ca9170b784a74a1076cb7702863f008aec8
n-gram-hub <54742855+n-gram-hub@users.noreply.github.com>
2023-04-01 14:36:26 +0200

    Create TweenPath2D.gd
    Godot 4, tween over Path2D tweening the progress_ratio property

---
+++ TweenPath2D.gd
@@ -0,0 +1,27 @@
+func get_progress_at_point(point_coords:Vector2) -> float:
+	
+	var baked_points = curve.get_baked_points()
+	
+	var total_pixel_length = 0.0
+	var point_index = baked_points.find(point_coords)
+	
+	for i in range(0, point_index):
+		
+		var p2 = baked_points[i+1]
+		var p1 = baked_points[i]
+		
+		# sqrt(x2-x1)^2+(y2-y1)^2
+		var d = sqrt(pow(p2[0] - p1[0], 2) + pow(p2[1] - p1[1], 2))
+		total_pixel_length += d
+	
+    return total_pixel_length
+  
+ # usage
+ 
+var point_coords = Vector2(883.436, 568)
+var dist = get_progress_at_point(point_coords)
+var baked_length = curve.get_baked_length()
+var ratio = dist/baked_length
+
+var tween = get_tree().create_tween()
+tween.tween_property($"../Path2D/PathFollow2D", "progress_ratio", ratio, 2.0)
