8f2327353e0157bec38ea3db9dee0de789234f3e
n-gram-hub <54742855+n-gram-hub@users.noreply.github.com>
2023-04-03 15:19:53 +0200

    Update TweenPath2D.gd

--- TweenPath2D.gd
+++ TweenPath2D.gd
@@ -4,24 +4,29 @@ func get_progress_at_point(point_coords:Vector2) -> float:
 	
 	var baked_points = curve.get_baked_points()
 	
-	var total_pixel_length = 0.0
+	var baked_length = 0.0
 	var point_index = baked_points.find(point_coords)
+	
+	if point_index == -1:
+		return float(-1)
 	
 	for i in range(0, point_index):
 		
 		var p2 = baked_points[i+1]
 		var p1 = baked_points[i]
 		
-		# sqrt(x2-x1)^2+(y2-y1)^2
+		# sqrt[(x2-x1)^2+(y2-y1)^2]
 		var d = sqrt(pow(p2[0] - p1[0], 2) + pow(p2[1] - p1[1], 2))
-		total_pixel_length += d
+		baked_length += d
 	
-    return total_pixel_length
+	return baked_length
   
  # usage
  
+# vector should not be hardcoded, anyway
 var point_coords = Vector2(883.436, 568)
 var dist = get_progress_at_point(point_coords)
+# test for -1.0 before using
 var baked_length = curve.get_baked_length()
 var ratio = dist/baked_length
 
